e9a4acce1cd539bd0407decee8c8e33315f7be8a
simokhounti <larbikhounti@gmail.com>
2021-01-28 00:29:55 +0100

    database

--- database/database.sql
+++ database/database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 21, 2021 at 01:42 PM
+-- Generation Time: Jan 28, 2021 at 12:26 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.0
 
@@ -105,7 +105,23 @@ CREATE TABLE `items` (
 
 INSERT INTO `items` (`id`, `title`, `descreption`, `price`, `quantity`, `date_added`, `primary_image`) VALUES
 (1, 'new shoes 2020 vans', '100% plastic free environment protected original ', 99, 12, '2021-01-10 15:57:20', 'https://pngimg.com/uploads/running_shoes/running_shoes_PNG5816.png'),
-(2, 'new shoes 2020 vans', '100% plastic free environment protected original ', 99.99, 12, '2021-01-10 15:57:30', 'https://pngimg.com/uploads/running_shoes/running_shoes_PNG5816.png');
+(2, 'new shoes 2020 vans', '100% plastic free environment protected original ', 99.99, 12, '2021-01-10 15:57:30', 'https://pngimg.com/uploads/running_shoes/running_shoes_PNG5816.png'),
+(3, 'my products 1', 'fezfzef', 0.01, 1, '2021-01-27 22:34:07', 'C:/xampp2/htdocs/ecomerce/uploads/1611786847fefe.PNG'),
+(4, 'my products 1', 'fezfzef', 0.01, 1, '2021-01-27 22:38:22', 'localhost/ecomerce/uploads/1611787102fefe.PNG'),
+(5, 'my products 1', 'fezfzef', 0.01, 1, '2021-01-27 22:39:16', 'http://localhost/ecomerce/uploads/1611787156fefe.PNG'),
+(6, 'my products 1', 'fezfzef', 0.01, 1, '2021-01-27 22:46:11', 'http://localhost/ecomerce/uploads/1611787571fefe.PNG'),
+(7, 'my products 1', 'fezfzef', 0.01, 1, '2021-01-27 22:50:33', 'http://localhost/ecomerce/uploads/1611787833fefe.PNG'),
+(8, 'my products 1', 'fezfzef', 0.01, 1, '2021-01-27 22:56:02', 'http://localhost/ecomerce/uploads/1611788162fefe.PNG'),
+(9, 'my products 1', 'fezfzef', 0.01, 1, '2021-01-27 22:58:52', 'http://localhost/ecomerce/uploads/1611788332fefe.PNG'),
+(10, 'my products 1', 'fezfzef', 0.01, 1, '2021-01-27 22:59:53', 'http://localhost/ecomerce/uploads/1611788393fefe.PNG'),
+(11, 'my products 1', 'fezfzef', 0.01, 1, '2021-01-27 23:03:11', 'http://localhost/ecomerce/uploads/1611788591fefe.PNG'),
+(12, 'my products 1', 'fezfzef', 0.01, 1, '2021-01-27 23:10:36', 'http://localhost/ecomerce/uploads/1611789036fefe.PNG'),
+(13, 'my products 1', 'fezfzef', 0.01, 1, '2021-01-27 23:12:18', 'http://localhost/ecomerce/uploads/1611789138fefe.PNG'),
+(14, 'my products 1', 'fezfzef', 0.01, 1, '2021-01-27 23:14:46', 'http://localhost/ecomerce/uploads/1611789286fefe.PNG'),
+(15, 'my products 1', 'fezfzef', 0.01, 1, '2021-01-27 23:16:16', 'http://localhost/ecomerce/uploads/1611789376fefe.PNG'),
+(16, 'my products 1', 'fezfzef', 0.01, 1, '2021-01-27 23:17:30', 'http://localhost/ecomerce/uploads/1611789450fefe.PNG'),
+(17, 'my products 1', 'fzefzefzef', 1.5, 1, '2021-01-27 23:18:28', 'http://localhost/ecomerce/uploads/1611789508f.jpeg'),
+(18, 'my products 1', 'fzefzefzef', 1.5, 1, '2021-01-27 23:20:40', 'http://localhost/ecomerce/uploads/1611789640f.jpeg');
 
 -- --------------------------------------------------------
 
@@ -124,8 +140,9 @@ CREATE TABLE `item_category` (
 --
 
 INSERT INTO `item_category` (`id`, `items_id`, `category_id`) VALUES
-(1, 1, 4),
-(2, 1, 3);
+(3, 18, 1),
+(4, 18, 2),
+(5, 18, 3);
 
 -- --------------------------------------------------------
 
@@ -144,27 +161,26 @@ CREATE TABLE `item_color` (
 --
 
 INSERT INTO `item_color` (`id`, `item_id`, `color_id`) VALUES
-(1, 2, 1),
-(2, 2, 2);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `item_picture`
---
-
-CREATE TABLE `item_picture` (
-  `id` int(11) NOT NULL,
-  `image_id` int(11) NOT NULL,
-  `item_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `item_picture`
---
-
-INSERT INTO `item_picture` (`id`, `image_id`, `item_id`) VALUES
-(1, 1, 2);
+(3, 13, 1),
+(4, 13, 2),
+(5, 13, 3),
+(6, 14, 1),
+(7, 14, 2),
+(8, 14, 3),
+(9, 15, 1),
+(10, 15, 2),
+(11, 15, 3),
+(12, 16, 1),
+(13, 16, 2),
+(14, 16, 3),
+(15, 17, 1),
+(16, 17, 2),
+(17, 17, 3),
+(18, 17, 4),
+(19, 18, 1),
+(20, 18, 2),
+(21, 18, 3),
+(22, 18, 4);
 
 -- --------------------------------------------------------
 
@@ -188,15 +204,33 @@ CREATE TABLE `orders` (
 
 CREATE TABLE `pictures` (
   `id` int(11) NOT NULL,
-  `url` varchar(255) NOT NULL
+  `url` varchar(255) NOT NULL,
+  `product_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `pictures`
 --
 
-INSERT INTO `pictures` (`id`, `url`) VALUES
-(1, 'https://assets.ajio.com/medias/sys_master/root/hd4/h99/14092964397086/-1117Wx1400H-460455972-black-MODEL.jpg');
+INSERT INTO `pictures` (`id`, `url`, `product_id`) VALUES
+(5, 'http://localhost/ecomerce/uploads/1611788393f.jpeg', 10),
+(6, 'http://localhost/ecomerce/uploads/1611788393flat-running-man-athletic-boy-run-animation-frames-sequence-runner-male-tracksuit-legs-animations-cartoon-vector-flat-running-122492103.jpg', 10),
+(7, 'http://localhost/ecomerce/uploads/1611788591f.jpeg', 11),
+(8, 'http://localhost/ecomerce/uploads/1611788591flat-running-man-athletic-boy-run-animation-frames-sequence-runner-male-tracksuit-legs-animations-cartoon-vector-flat-running-122492103.jpg', 11),
+(9, 'http://localhost/ecomerce/uploads/1611789036f.jpeg', 12),
+(10, 'http://localhost/ecomerce/uploads/1611789036flat-running-man-athletic-boy-run-animation-frames-sequence-runner-male-tracksuit-legs-animations-cartoon-vector-flat-running-122492103.jpg', 12),
+(11, 'http://localhost/ecomerce/uploads/1611789138f.jpeg', 13),
+(12, 'http://localhost/ecomerce/uploads/1611789138flat-running-man-athletic-boy-run-animation-frames-sequence-runner-male-tracksuit-legs-animations-cartoon-vector-flat-running-122492103.jpg', 13),
+(13, 'http://localhost/ecomerce/uploads/1611789286f.jpeg', 14),
+(14, 'http://localhost/ecomerce/uploads/1611789286flat-running-man-athletic-boy-run-animation-frames-sequence-runner-male-tracksuit-legs-animations-cartoon-vector-flat-running-122492103.jpg', 14),
+(15, 'http://localhost/ecomerce/uploads/1611789376f.jpeg', 15),
+(16, 'http://localhost/ecomerce/uploads/1611789376flat-running-man-athletic-boy-run-animation-frames-sequence-runner-male-tracksuit-legs-animations-cartoon-vector-flat-running-122492103.jpg', 15),
+(17, 'http://localhost/ecomerce/uploads/1611789450f.jpeg', 16),
+(18, 'http://localhost/ecomerce/uploads/1611789450flat-running-man-athletic-boy-run-animation-frames-sequence-runner-male-tracksuit-legs-animations-cartoon-vector-flat-running-122492103.jpg', 16),
+(19, 'http://localhost/ecomerce/uploads/1611789508fefe.PNG', 17),
+(20, 'http://localhost/ecomerce/uploads/1611789508flat-running-man-athletic-boy-run-animation-frames-sequence-runner-male-tracksuit-legs-animations-cartoon-vector-flat-running-122492103.jpg', 17),
+(21, 'http://localhost/ecomerce/uploads/1611789640fefe.PNG', 18),
+(22, 'http://localhost/ecomerce/uploads/1611789640flat-running-man-athletic-boy-run-animation-frames-sequence-runner-male-tracksuit-legs-animations-cartoon-vector-flat-running-122492103.jpg', 18);
 
 -- --------------------------------------------------------
 
@@ -269,14 +303,6 @@ ALTER TABLE `item_color`
   ADD KEY `item_id` (`item_id`);
 
 --
--- Indexes for table `item_picture`
---
-ALTER TABLE `item_picture`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `image_id` (`image_id`),
-  ADD KEY `item_id` (`item_id`);
-
---
 -- Indexes for table `orders`
 --
 ALTER TABLE `orders`
@@ -288,7 +314,8 @@ ALTER TABLE `orders`
 -- Indexes for table `pictures`
 --
 ALTER TABLE `pictures`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `itemo` (`product_id`);
 
 --
 -- Indexes for table `users`
@@ -323,25 +350,19 @@ ALTER TABLE `colors`
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `item_category`
 --
 ALTER TABLE `item_category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `item_color`
 --
 ALTER TABLE `item_color`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `item_picture`
---
-ALTER TABLE `item_picture`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `orders`
@@ -353,7 +374,7 @@ ALTER TABLE `orders`
 -- AUTO_INCREMENT for table `pictures`
 --
 ALTER TABLE `pictures`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -380,18 +401,17 @@ ALTER TABLE `item_color`
   ADD CONSTRAINT `item_color_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`);
 
 --
--- Constraints for table `item_picture`
---
-ALTER TABLE `item_picture`
-  ADD CONSTRAINT `item_picture_ibfk_1` FOREIGN KEY (`image_id`) REFERENCES `pictures` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `item_picture_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
 -- Constraints for table `orders`
 --
 ALTER TABLE `orders`
   ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`) ON UPDATE CASCADE,
   ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `pictures`
+--
+ALTER TABLE `pictures`
+  ADD CONSTRAINT `itemo` FOREIGN KEY (`product_id`) REFERENCES `items` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
